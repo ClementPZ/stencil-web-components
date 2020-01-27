fbdbe91b3a67a785b82379be2de1f384a802742e
ClementPZ <clementpz@MacBook-Air-de-ClementPZ.local>
2020-01-27 14:24:17 -0600

    start styling for side drawer

--- src/components/my-component/side-drawer/side-drawer.tsx
+++ src/components/my-component/side-drawer/side-drawer.tsx
@@ -2,13 +2,15 @@ import { Component, h } from '@stencil/core';
 
 @Component({
     tag: "my-side-drawer",
+    styleUrl: "./side-drawer.css",
+    shadow: true
 })
 
 export class SideDrawer {
     render() {
         return (
             <div>
-                <h1>My Side Drawer</h1>
+                <h1>MY SIDE DRAWER</h1>
             </div>
         );
     }

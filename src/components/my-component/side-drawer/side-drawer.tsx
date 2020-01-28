5e75e422911b656411b1f8cb1f29c0f1a823ef5f
ClementPZ <clementpz@MacBook-Air-de-ClementPZ.local>
2020-01-28 16:37:17 -0600

    rendering issue fixed, was due to div nomination conflict, renaming div fro render() return OK

--- src/components/my-component/side-drawer/side-drawer.tsx
+++ src/components/my-component/side-drawer/side-drawer.tsx
@@ -13,36 +13,35 @@ export class SideDrawer {
     closeDrawer() {
         this.open = false;
     }
-
     changeTab(content) {
         console.log(content);
     }
-
     render() {
         let mainContent = <slot />;
         mainContent = (
-            <div>
+            <div id="contact-informations">
                 <h2>Our contacts</h2>
+                <h3>You can reach us here:</h3>
                 <ul>
                     <li>our phone  5538292828</li>
                     <li>our email: <a href="mailto:cleent@gen.com">mailto:cleent@gen.com</a></li>
                 </ul>
             </div>
-        )
+        );
         return (                
-        <div>
+        <aside>
             <header>
                 <h1>{this.title}</h1>
                 <button onClick={this.closeDrawer.bind(this)}>X</button>
             </header>
             <section id="tabs">
                 <button class="active" onClick={this.changeTab.bind(this, "nav")}>navigation</button>
-                <button onClick={this.changeTab.bind(this, "contacts")}>contact</button>
+                <button onClick={this.changeTab.bind(this, "contact")}>contact</button>
             </section>
             <main>
                 {mainContent}
             </main>
-        </div>
+        </aside>
         );
     }
 }
